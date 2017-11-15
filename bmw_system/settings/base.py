caaff815658c4d79bbd08ab111c755a6263dd9f1
darren <darren.cy.liang@gmail.com>
2017-11-15 17:48:44 +0800

    add channel module and update requirements

--- bmw_system/settings/base.py
+++ bmw_system/settings/base.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    'channels',
     "solo",
     'bmw.apps.BmwConfig',
     'rest_framework.authtoken',
