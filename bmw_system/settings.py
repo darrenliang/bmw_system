dc7feb0bd46b5900589838f5d063b446653a2e71
darren <darren.cy.liang@gmail.com>
2017-11-19 00:55:31 +0800

    modify the debug mode to True

--- bmw_system/settings.py
+++ bmw_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!p_&x!rv9(u*aaeubn^5vwjri=82d72*4$skea7&v2knwapf2_'
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 # LOGGING["loggers"]['']["level"] = "DEBUG"
