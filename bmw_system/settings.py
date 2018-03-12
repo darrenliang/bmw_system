c9932fd53b8455c2b2f781ce262f8a079664fc05
darren <darren.cy.liang@gmail.com>
2018-03-13 00:48:17 +0800

    update settings

--- bmw_system/settings.py
+++ bmw_system/settings.py
@@ -27,12 +27,24 @@ ALLOWED_HOSTS = ["*"]
 
 # LOGGING["loggers"]['']["level"] = "DEBUG"
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "evproject",
+        'USER': "admin",
+        'PASSWORD': "obey",
+        'HOST': "127.0.0.1",
+        'PORT': "",
     }
 }
+
 
 # Application definition
 
